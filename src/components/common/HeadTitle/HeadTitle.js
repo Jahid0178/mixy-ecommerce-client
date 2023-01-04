9976090bb4183f9d310f8a3b70392abff0d7d9b3
Zahidul Haque <jahidulhaque767@gmail.com>
2023-01-05 02:46:20 +0600

    added head title component

---
+++ src/components/common/HeadTitle/HeadTitle.js
@@ -0,0 +1,11 @@
+const HeadTitle = () => {
+  return (
+    <>
+      <title>Home</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  );
+};
+
+export default HeadTitle;
