f8d1d4fc71a7053f899ce778833de149b930e409
Zahidul Haque <jahidulhaque767@gmail.com>
2023-02-05 01:34:39 +0600

    profile page user form complete

--- src/pages/user/profile/index.js
+++ src/pages/user/profile/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { Col, Empty, Row, Typography } from "antd";
 import styles from "./style.module.css";
 import Image from "next/image";
@@ -10,6 +11,9 @@ const { Paragraph } = Typography;
 const Profile = () => {
   const { userInfo } = useAuth();
   const { email, photoURL, displayName } = userInfo;
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(userInfo);
   return (
     <div>
       <Row gutter={20}>
@@ -23,8 +27,8 @@ const Profile = () => {
                 <>
                   <Image
                     src={photoURL}
-                    width={100}
-                    height={100}
+                    width={150}
+                    height={150}
                     alt={displayName + " " + "Profile Image"}
                   />
                   <Paragraph
@@ -46,6 +50,76 @@ const Profile = () => {
             <Title level={4} style={{ marginBottom: 0 }}>
               Profile Information
             </Title>
+            <div className={styles.formContainer}>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={styles.formGroup}>
+                  <label htmlFor="fullName">Full Name</label>
+                  <br />
+                  <input
+                    {...register("fullName", {
+                      required: true,
+                    })}
+                    placeholder="Full Name"
+                    id="fullName"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="phone">Phone Number</label>
+                  <br />
+                  <input
+                    {...register("phoneNumber", { required: true })}
+                    placeholder="Phone Number"
+                    id="phone"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="address">Address</label>
+                  <br />
+                  <input
+                    {...register("address", { required: true })}
+                    placeholder="Address"
+                    id="address"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="post">Post Code</label>
+                  <br />
+                  <input
+                    {...register("postCode", { required: true })}
+                    placeholder="Post Code"
+                    id="post"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="state">State</label>
+                  <br />
+                  <input
+                    {...register("state", { required: true })}
+                    placeholder="State"
+                    id="state"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="email">Email</label>
+                  <br />
+                  <input
+                    {...register("email", { required: true })}
+                    placeholder="Email"
+                    id="email"
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label htmlFor="country">Country</label>
+                  <br />
+                  <input
+                    {...register("country", { required: true })}
+                    placeholder="Country"
+                    id="country"
+                  />
+                </div>
+                <input className={styles.formSubmitBtn} type="submit" />
+              </form>
+            </div>
           </div>
         </Col>
       </Row>
